b4b4e59561e67f2febd31960367a8268b87f28c5
Bane Vujanovic <branislavvujanovic4@gmail.com>
2019-11-20 13:42:40 +0100

    6th commit - adding comments in files

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+
+/* Function witch call two functions and run them after clicking on the button START GAME */
+function callTwo(){
+    getLvlValue();
+    startGame();
+}
+
+/* Function witch get a Level of playing */
+function getLvlValue(){
+    var value = document.getElementById("selectLevel").value;
+    console.log(value);
+}
+
+/* This function start a Game */
 function startGame(){
     document.getElementById("startDiv").classList.add("hidden");
     document.getElementById("afterStartsDiv").classList.remove("hidden");
